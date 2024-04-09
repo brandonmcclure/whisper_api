3b50bdd744831987aae0372097c8cea49710ab53
Brandon McClure <brandonmcclure89@gmail.com>
2024-04-09 10:06:38 -0600

    switch from camel case to _ remove unused var

--- tests/test_mymod.py
+++ tests/test_mymod.py
@@ -3,12 +3,11 @@ import io
 import numpy
 from scipy.io.wavfile import read
 from mymod import perform_stt
-class Testperform_stt(unittest.TestCase): 
+class test_perform_stt(unittest.TestCase): 
     
     # Do a test that we can perform STT on an example wav file    
     def test_perform_stt_valid_input(self):
         valid_wav = read("./tests/mary_had_a_lamb.wav")
-        valid_wav_as_array = numpy.array(valid_wav[1],dtype=float)    
         output = perform_stt(valid_wav,'small.en')
 
         self.assertEqual(output,'I want to make sure that you have a good time. I want to make sure that you have a good time. I want to make sure that you have a good time. I want to make sure that you have a good time. I want to make sure that you have a good time.')
